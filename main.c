3e96717817a6f0768cf323d9767d51c2255a8539
Marcin Ryżewski <marcin.ryzewskii@gmail.com>
2025-09-05 22:23:04 +0200

    main.c: change main file for new test

--- main.c
+++ main.c
@@ -3,24 +3,22 @@
 
 #include "app.h"
 #include "./raylib/src/raylib.h"
+#include "ui.h" 
 
 #define WIN_WIDTH 1200
 #define WIN_HEIGHT 1000
 
 int main() {
-    int ret = app_init();
-    if (ret != 0) {
-        fprintf(stderr, "Err: app cant be init (%d)", ret);
-        return 1;
-    }
+    // int ret = app_init();
+    // if (ret != 0) {
+    //     fprintf(stderr, "Err: app cant be init (%d)", ret);
+    //     return 1;
+    // }
 
     InitWindow(WIN_WIDTH, WIN_HEIGHT, "ez-com");
     while (!WindowShouldClose()) {
-        app_update();
-
-        BeginDrawing();
-        ClearBackground(SKYBLUE);
-        EndDrawing();
+        ui_event_loop(NULL);
+        ui_render_frame(NULL);
     }
 
     return 0;
